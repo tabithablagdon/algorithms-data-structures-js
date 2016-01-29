5fd8fec361075d876b6c458fbfa47b46f4f2fc11
Tee Blagdon <tjlay23@gmail.com>
2016-01-28 22:27:57 -0800

    Create capitalize-words.js

---
+++ capitalize-words.js
@@ -0,0 +1,14 @@
+// The Capitalize Words function takes any sentence and capitalizes the first letter of each word
+
+function capitalizeWords(str) {
+  var strArray = str.toLowerCase().split(" ");
+  for (var i = 0; i < strArray.length; i++){
+    var word = strArray[i];
+    word = word.charAt(0).toUpperCase() + word.slice(1);
+    strArray[i] = word;
+  }
+  var newStr = strArray.join(" ");
+  return newStr;
+};
+
+capitalizeWords("I'm a little tea pot");
