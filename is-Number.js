53c6721fcb164bf6f225ec49c967b4f872945e86
Tee Blagdon <tjlay23@gmail.com>
2016-02-09 20:44:39 -0800

    Update is-Number.js

--- is-Number.js
+++ is-Number.js
@@ -1,17 +1,16 @@
 function isNumber(num){
 
-    if ((typeof num === 'string' || typeof num === 'number') && !isNaN(num) && num !== '') {
-        console.log("True!  The number you entered is " + num);
+    if ((typeof num === 'string' || typeof num === 'number') && !isNaN(num) && num !== "") {
+        console.log("True!  You entered " + num);
     }
     else {
-        console.log("False!  You entered the faulty value " + num + ".  Enter a number.");
+        console.log("False!  You entered " + num);
     }
-
 };
 
 // Test cases
 
-isNumber(0);  //true
+isNumber(0);  // true
 isNumber(-5); // true
 isNumber("5"); // true
 isNumber(5.234); // true
@@ -25,9 +24,3 @@ isNumber(undefined); // false
 isNumber('one');  // false
 isNumber([]);  // false
 isNumber(true);  // false
-
-// How do I fix these??
-
-isNumber(%); // unexpected token %
-isNumber(5 + 3); // Invalid flags supplied to RegExp constructor 'true'
-isNumber(undeclaredVariable); //referenceError: undeclaredVariable is not defined - how to fix???
