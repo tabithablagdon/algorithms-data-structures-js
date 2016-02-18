92bbcb5e20c91b0e9c185a2d63772483f8c15412
Tee Blagdon <tjlay23@gmail.com>
2016-02-17 16:45:23 -0800

    Create is-array-like.js

---
+++ Arrays/is-array-like.js
@@ -0,0 +1,16 @@
+// isArrayLike() checks to see if the argument provided is an array-like object
+// checks for typeof object and length properties
+
+
+function isArrayLike(arr) {
+    if (arr && 
+        typeof arr === "object" &&
+        isFinite(arr.length) && 
+        arr.length >= 0 && 
+        arr.length === Math.floor(arr.length)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
