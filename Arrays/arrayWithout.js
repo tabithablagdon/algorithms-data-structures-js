581951bcd2b273c092ebaf3bd976c8f690c1d436
Tee Blagdon <tjlay23@gmail.com>
2016-03-09 20:18:21 -0800

    Create arrayWithout.js
    arrayWithout() takes an array list input and an unlimited number of values.  Returns  new array with those values removed.

---
+++ Arrays/arrayWithout.js
@@ -0,0 +1,17 @@
+/* arrayWithout() takes an array list input and an unlimited number of values.  Returns  new array with those values removed. */
+
+var arrayWithout = function(array, ...args) {
+    var theArgs = args;
+    if (Array.isArray(array)) {
+        return array.filter(function(item) {
+          return theArgs.indexOf(item) < 0 ? true : false; 
+        });
+    }
+    else {
+        return "Enter an array";
+    }
+    
+}
+
+arrayWithout([10, 12, 100, 2, 3, 4, 0, undefined, 15], 0, 3, undefined);
+// => [ 10, 12, 100, 2, 4, 15 ]
