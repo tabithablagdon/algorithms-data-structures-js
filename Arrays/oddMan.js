545aa9335f24369e518207ec2a25a657ee9f01f2
Tee Blagdon <tjlay23@gmail.com>
2016-04-05 17:21:07 -0700

    Create oddMan.js

---
+++ Arrays/oddMan.js
@@ -0,0 +1,43 @@
+/* 
+
+Time Complexity: O(N**2)
+TO DO: Re-write in O(N) complexity
+
+oddMan(A) - given an array A consisting of N integers returns the value of the unpaired element - the oddman.
+
+For example, given array A such that:
+
+  A[0] = 9  A[1] = 3  A[2] = 9
+  A[3] = 3  A[4] = 9  A[5] = 7
+  A[6] = 9
+  
+the function should return 7.
+
+Assume that:
+ - N is an odd integer within the range [1..1,000,000];
+ - each element of array A is an integer within the range [1..1,000,000,000];
+ - all but one of the values in A occur an even number of times.
+*/
+
+var oddMan = function(A) {
+  var oddman;
+  var index;
+  var currentNum;
+  
+  while (A.length > 1) {
+  	currentNum = A.shift();
+  	index = A.indexOf(currentNum);
+  	if (index < 0) {
+  	  oddman = currentNum;
+  	  break;
+  	} else {
+  	  A.splice(index, 1);	
+  	}
+  }
+  return oddman ? oddman : A[0];
+};
+
+var arr = [2, 3, 4, 5, 5, 1, 4, 3, 2];
+
+oddMan(arr);
+// => 1
