b85155fe9dc384128e5dfb8340cc6f2b0af367f4
Tee Blagdon <tjlay23@gmail.com>
2016-04-13 16:31:51 -0700

    Create dequeue.js

---
+++ data-structures/dequeue.js
@@ -0,0 +1,47 @@
+// Array-Implementation of a double-ended queue
+
+var Dequeue = function() {
+  this.dataStore = [];
+};
+
+Dequeue.prototype = (function() {
+	
+ var enqueueBack = function(element) {
+   this.dataStore.push(element);	
+ };
+ 
+ var enqueueFront = function(element) {
+   this.dataStore.unshift(element);	
+ };
+ 
+ var dequeueBack = function() {
+   return this.dataStore.pop();
+ };
+ 
+ var dequeueFront = function() {
+   return this.dataStore.shift();
+ };
+
+ var front = function() {
+   return this.dataStore[0];
+ };
+ 
+ var back = function() {
+   return this.dataStore[this.dataStore.length - 1];	
+ };
+ 
+ var empty = function() {
+   return this.dataStore.length === 0 ? true : false;	
+ };
+
+  return {
+	enqueueFront: enqueueFront,
+	enqueueBack: enqueueBack,
+	dequeueFront: dequeueFront,
+	dequeueBack: dequeueBack,
+	front: front,
+	back: back,
+	empty: empty
+  };
+
+})();
