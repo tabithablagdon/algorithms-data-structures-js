30f91ac651eb8d4675e8917b1657c3b8d1997a2c
Tee Blagdon <tjlay23@gmail.com>
2016-04-13 22:46:56 -0700

    Create doublyLinkedList.js

---
+++ data-structures/doublyLinkedList.js
@@ -0,0 +1,81 @@
+// Doubly Linked List Design w/ two classes Node and LList
+
+var Node = function(element) {
+  this.element = element;
+  this.next = null;
+  this.previous = null;
+};
+
+// Linked List Constructor
+var LList = function() {
+  this.head = new Node('head');
+};
+
+LList.prototype = {
+  constructor: 'LList',
+  find: function(item) {
+  	var currNode = this.head;
+  	while (currNode.element !== item) {
+  	  currNode = currNode.next;	
+  	}
+  	return currNode;
+  }, 
+  findLast: function() {
+  	var current = this.head;
+  	while (current.next) {
+  	  current = current.next;	
+  	}
+  	return current;
+  }, 
+  insert: function(newElement, item) {
+  	var current = this.find(item);
+  	var newNode = new Node(newElement);
+  	newNode.next = current.next;
+  	newNode.previous = current;
+  	current.next = newNode;
+  }, 
+  remove: function(item) {
+  	var current = this.find(item);
+  	if (current) {
+  	  current.previous.next = current.next;
+  	  current.next.previous = current.previous;
+  	  current.next = null;
+  	  current.previous = null;
+  	}
+  }, 
+  display: function() {
+  	var currNode = this.head;
+  	while (currNode.next) {
+  	  console.log(currNode.next.element);
+  	  currNode = currNode.next;
+  	}
+  }, 
+  displayReverse: function() {
+  	var currNode = this.findLast();
+  	while (currNode.previous) {
+  	  console.log(currNode.element);
+  	  currNode = currNode.previous;
+  	}
+  }
+};
+
+var cities = new LList();
+cities.insert('Piscataway', 'head');
+cities.insert('Sacramento', 'Piscataway');
+cities.insert('San Francisco', 'Sacramento');
+cities.insert('Santa Monica', 'San Francisco');
+cities.display('Sacramento');
+/*
+=> Sacramento
+Santa Monica
+San Francisco
+Piscataway
+*/
+cities.remove('Sacramento');
+
+cities.displayReverse();
+/*
+=> Santa Monica
+San Francisco
+Piscataway
+*/
