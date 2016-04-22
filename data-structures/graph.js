08e1a500511f473766003dea41915796b72432f2
Tee Blagdon <tjlay23@gmail.com>
2016-04-22 11:15:34 -0700

    Update graph.js

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -1,4 +1,4 @@
-// simple array implementation of Graph class with depth-first search
+// simple array implementation of Graph class with depth-first search and breadth-first search
 
 var Graph = function(v) { // v = # of vertices in graph
   this.vertices = v;
@@ -21,7 +21,25 @@ Graph.prototype = (function() {
   	this.edges++;
   };
   
-  var depthSearch = function(vertex) {
+  var bfs = function(vertex) {
+    var queue = [];
+    this.marked[vertex] = true;
+    queue.push(vertex);
+    while (queue.length > 0) {
+      var v = queue.shift();
+      if (v !== undefined) {
+        console.log('Visited vertex: ' + v);
+      }
+      for (var i = 0; i < this.adj[v].length; i++) {
+        if (!this.marked[this.adj[v][i]]) {
+          this.marked[this.adj[v][i]] = true;
+          queue.push(this.adj[v][i]);
+        }	
+      }
+    }	
+  };
+  
+  var dfs = function(vertex) {
     this.marked[vertex] = true;
     if (this.adj[vertex] !== undefined) {
       console.log('visited vertex: ' + vertex);
@@ -48,7 +66,8 @@ Graph.prototype = (function() {
   return {
   	constructor: Graph, 
   	addEdge: addEdge, 
-  	depthSearch: depthSearch, 
+  	bfs: bfs, 
+  	dfs: dfs, 
   	showGraph: showGraph
   };
   
@@ -68,11 +87,20 @@ g.showGraph();
 3 ->  1
 4 ->  2
 */
-g.depthSearch(0);
+g.dfs(0);
 /*
 visited vertex: 0
 visited vertex: 1
 visited vertex: 3
 visited vertex: 2
 visited vertex: 4
+*/
+
+g.bfs(0);
+/*
+=> Visited vertex: 0
+Visited vertex: 1
+Visited vertex: 2
+Visited vertex: 3
+Visited vertex: 4
 */
