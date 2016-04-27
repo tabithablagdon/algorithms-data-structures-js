de44d7d7976ff17528c6e1d9d4c9bf440dd0a4cd
Tee Blagdon <tjlay23@gmail.com>
2016-04-26 19:01:16 -0700

    Create insertionSort.js

---
+++ sorting/insertionSort.js
@@ -0,0 +1,24 @@
+/*
+O(N^2)
+insertion sort works by moving larger array elements to the right to make room for the smaller elements
+on the left side of the array
+*/
+
+var insertionSort = function(collection) {
+  var temp;
+  for (var i = 1; i < collection.length; i++) {
+    for (var j = 0; j < i; j++) {
+      if (collection[i] < collection[j]) {
+      	temp = collection[j];
+      	collection[j] = collection[i];
+      	collection[i] = temp;
+      }
+    }
+    console.log(collection);
+  }
+  return collection;
+};
+
+var arr = [8, 4, 1, 0, 2];
+insertionSort(arr);
+// => [ 0, 1, 2, 4, 8 ]
