dfd3535597f0e68142dc5b7c9e9fccfa73aaaaf9
Tee Blagdon <tjlay23@gmail.com>
2016-04-28 12:27:39 -0700

    Update largest-of-four-numbers-array.js

--- Arrays/largest-of-four-numbers-array.js
+++ Arrays/largest-of-four-numbers-array.js
@@ -1,18 +1,12 @@
 // The largestOfFour function returns an array containing the largest of 4 numbers in a nested array of numbers
 
 function largestOfFour(arr) {
-  // You can do this!
-  
-  var newArray =[];
-  
-  for (var i = 0; i < arr.length; i++) {
-    arr[i].sort(function (a, b) {
-      return a < b;
-    });
-    newArray.push(arr[i][0]);
-  }
-  
-  return newArray;
+  return arr.map(function(set) {
+  	return set.reduce(function(a, b) {
+  	  return a > b ? a : b;	
+  	});
+  });
 }
 
 largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+// [ 5, 27, 39, 1001 ]
