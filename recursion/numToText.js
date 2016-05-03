6f55a1e408d9e8730f568d9a79defc732fc4155a
Tee Blagdon <tjlay23@gmail.com>
2016-05-03 11:26:29 -0700

    Update numToText.js

--- recursion/numToText.js
+++ recursion/numToText.js
@@ -35,3 +35,42 @@ var numToText = function(str) {
 
 numToText('I bought 7 apples and 3 oranges');
 // 'I bought seven apples and three oranges'
+
+
+/*
+Create a function using recursion called numToText which takes a string and returns a new string with all numeric numbers from the input string converted to their corresponding text words. You can assume that the numbers in the string are single digit numbers.
+
+*/
+
+var numToText2 = function(str) {
+  var nums = {
+    1: 'one', 
+    2: 'two', 
+    3: 'three', 
+    4: 'four', 
+    5: 'five', 
+    6: 'six', 
+    7: 'seven', 
+    8: 'eight', 
+    9: 'nine', 
+    0: 'zero'
+  };
+  
+  var newStr = '';
+  
+  if (str.length === 0) {
+  	return newStr;
+  } else {
+    var currentChar = str.charAt(0);
+    if (nums.hasOwnProperty(currentChar)) {
+      newStr += nums[currentChar];
+    } else {
+      newStr += currentChar;	
+    }
+  	return newStr.concat(numToText2(str.slice(1)));
+  }
+};
+
+
+numToText2('I bought 7 apples and 3 oranges');
+// 'I bought seven apples and three oranges'
