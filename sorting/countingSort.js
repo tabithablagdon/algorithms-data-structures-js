c8b45a38f8be25f78bf32b2383465539b22d9ba7
Tabitha Blagdon <tjlay23@gmail.com>
2016-05-06 16:06:41 -0700

    Update countingSort.js

--- sorting/countingSort.js
+++ sorting/countingSort.js
@@ -1,14 +1,11 @@
 var countingSort = function(arr, min, max) {
   var i, j = 0, count = [], result = [];
-
   for (i = min; i <= max; i++) {  // initialize count array
     count[i] = 0; 
   }
-
   for (i = 0; i <= arr.length; i++) { // create histogram of count of elements
     count[arr[i]]++; 	
   }
-
   for (i = min; i <= max; i++) { // sort integers in new array
     while (count[i] > 0) {
       result[j++] = i;
