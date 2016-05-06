b4f7b05915ecf7b955fbefc2e833f2c4bfbf2b09
Tabitha Blagdon <tjlay23@gmail.com>
2016-05-06 16:02:17 -0700

    Create countingSort.js

---
+++ sorting/countingSort.js
@@ -0,0 +1,22 @@
+var countingSort = function(arr, min, max) {
+  var i, j = 0, count = [], result = [];
+
+  for (i = min; i <= max; i++) {  // initialize count array
+    count[i] = 0; 
+  }
+
+  for (i = 0; i <= arr.length; i++) { // create histogram of count of elements
+    count[arr[i]]++; 	
+  }
+
+  for (i = min; i <= max; i++) { // sort integers in new array
+  	while (count[i] > 0) {
+  	  result[j++] = i;
+  	  count[i]--;
+  	}
+  }
+  return result;
+};
+
+countingSort([5, 3, 1, -1, 10, 5, 2], -1, 10);
+// => [ -1, 1, 2, 3, 5, 5, 10 ]
