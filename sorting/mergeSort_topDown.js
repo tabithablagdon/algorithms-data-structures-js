839c716a2ae1cbe3825ec31857a7545ed1bc84c3
Tabitha Blagdon <tjlay23@gmail.com>
2016-05-09 08:11:44 -0700

    Update mergeSort_topDown.js

--- sorting/mergeSort_topDown.js
+++ sorting/mergeSort_topDown.js
@@ -1,42 +1,47 @@
 // Top down implementation using recursion
-// This code can be wrapped in a MergeSort function - separated for simplicity
 
-var merge = function(left, right) {
-  var result = [];
-  var leftIndex = 0;
-  var rightIndex = 0;
-  
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] > right[rightIndex]) {
-      result.push(right[rightIndex]);
-      rightIndex++;
-    } else {
-      result.push(left[leftIndex]);
-      leftIndex++;
-    }
-  }
-  while (leftIndex < left.length) {
-    result.push(left[leftIndex]);
-    leftIndex++;
-  }
-  while (rightIndex < right.length) {
-    result.push(right[rightIndex]);
-    rightIndex++;
-  }
-  return result;
-}
-
-var mergeSort = function(arr) {
-  if (arr.length === 1) {
-    return arr;	
-  }
-  
-  var mid = arr.length / 2;
-  var left = arr.slice(0, mid);
-  var right = arr.slice(mid);
-  
-  return merge(mergeSort(left), mergeSort(right));
+var mergeSort = function(array) {
+ var merge = function(left, right) {
+   var result = []
+     , leftIndex = 0
+     , rightIndex = 0;
+   
+   while (leftIndex < left.length && rightIndex < right.length) {
+     if (left[leftIndex] < right[rightIndex]) {
+       result.push(left[leftIndex]);
+       leftIndex++;
+     } else {
+       result.push(right[rightIndex]);	
+       rightIndex++;
+     }
+   }
+   while (leftIndex < left.length) {
+     result.push(left[leftIndex]);
+     leftIndex++;
+   }
+   while (rightIndex < right.length) {
+     result.push(right[rightIndex]);
+     rightIndex++;
+   }
+   return result;
+ };
+ 
+ var mSort = function(arr) {
+   if (arr.length === 1) {
+     return arr;
+   }
+   var mid = arr.length/2;
+   var left = arr.slice(0, mid);
+   var right = arr.slice(mid);
+   
+   return merge(mSort(left), mSort(right));
+ };
+ 
+ return mSort(array);
 };
 
-mergeSort([2, 6, 1, 8, -1, 2, -7]);
-//=> [ -7, -1, 1, 2, 2, 6, 8 ]
+mergeSort([1, 2, 1203, 2, 10, -4]);
+// => [ -4, 1, 2, 2, 10, 1203 ]
+
+mergeSort(['twelve', 'two', 'one', 'four', 'seven']);
+// => [ 'four', 'one', 'seven', 'twelve', 'two' ]
