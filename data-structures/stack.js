63093382fb39ad08b455e211570a8475404e9948
Tabitha Blagdon <tjlay23@gmail.com>
2016-05-13 11:52:40 -0700

    Add isEmpty method and update pop & clear methods

--- data-structures/stack.js
+++ data-structures/stack.js
@@ -1,14 +1,15 @@
 /*
-STACKS Abstract Implementation using a Stack Constructor and revealing prototype pattern
+STACKS Abstract Implementation using a Stack Constructor and revealing prototype pattern - no array methods are used
  - LIFO methodology
-
-Primary Properties/Methods/Operations
+ 
+Primary Methods/Operations
  - top - top of stack
  - push() - Adding to a stack
  - pop() - Removing items from stack
  - peek() - looking at the top value of the stack, without removing like pop would
  - clear() - removes all elements from stack
  - length() - returns length of stack
+ - isEmpty() - returns boolean whether stack is empty or not
 */
 
 var Stack = function() {
@@ -23,11 +24,16 @@ Stack.prototype = (function(){
   };
   
   var pop = function() {
-  	return this.dataStore[--this.top];
+  	var popped = null;
+  	if (this.top > 0) {
+  	  popped = this.dataStore[--this.top];
+  	  delete this.dataStore[this.top];
+  	}
+  	return popped;
   };
   
   var peek = function() {
-  	return this.dataStore[this.top-1];
+    return this.top > 0 ? this.dataStore[this.top-1] : null;
   };
   
   var length = function() {
@@ -36,6 +42,11 @@ Stack.prototype = (function(){
   
   var clear = function() {
   	this.top = 0;
+  	this.dataStore = [];
+  };
+  
+  var isEmpty = function() {
+    return this.top < 1;	
   };
   
   return {
@@ -44,7 +55,8 @@ Stack.prototype = (function(){
     pop: pop,
     peek: peek,
     length: length,
-    clear: clear
+    clear: clear, 
+    isEmpty: isEmpty
   };
 })();
 
@@ -58,3 +70,9 @@ console.log(names.pop());
 // => Ed
 console.log(names.peek());
 // => Bee
+console.log(names.isEmpty());
+// => false
+console.log(names.length());
+// => 2
+names.clear();
+console.log(names);
