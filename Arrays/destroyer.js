f30e109a5eee13b50e275641d2a5688019c77570
Tabitha Blagdon <tjlay23@gmail.com>
2016-05-12 21:14:10 -0700

    Update destroyer.js

--- Arrays/destroyer.js
+++ Arrays/destroyer.js
@@ -2,16 +2,13 @@
 It will remove all elements from the initial array that are of the same value as these arguments. */
 
 function destroyer(arr) {
-
   var args = Array.prototype.slice.call(arguments);
   var array1 = args[0];
   var values = args.slice(1);
   
   return array1.filter(function(val) {
     return values.indexOf(val) < 0;
-   
   });
-  
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 3, 2);
