1363826fcd3e69c9e681d135c2b9c3634ce54631
Tabitha Blagdon <tjlay23@gmail.com>
2016-05-13 21:59:48 -0700

    Create array-binary-search.js

---
+++ recursion/array-binary-search.js
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+////// BINARY SEARCH ARRAY
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+
+// Given a sorted array of numbers in ascending order
+// Return index of 2nd argument num
+// If num is not in array, return -1
+// Implement using binary search and recursion
+
+var binarySearchArray = function(array, num) {
+  var mid = Math.floor(array.length/2);
+  if (num === array[mid]) {
+  	return mid;
+  } else if(num < array[mid]) {
+  	return sorted(array.slice(0, mid), num);
+  } else if (num > array[mid]) {
+    return sorted(array.slice(mid, array.length), num) + mid;
+  } else {
+    return -1;	
+  }
+};
+
+var sortedArray = [39, 41, 42, 43, 44, 45, 50, 52, 53, 63, 74, 77, 78, 83, 93];
+
+sorted(sortedArray, 63);
+// => 9
+
+sorted(sortedArray, 10);
+// => -1
