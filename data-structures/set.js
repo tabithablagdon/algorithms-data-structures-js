0903e85caed98efb09222e71c390ac24c85d7ebb
Tabitha Blagdon <tjlay23@gmail.com>
2016-05-15 22:35:29 -0700

    Update set.js

--- data-structures/set.js
+++ data-structures/set.js
@@ -1,3 +1,129 @@
+// Object implementation of a Set class
+
+var Set = function() {
+  var items = {};
+  var length = 0;
+  
+  this.has = function(value) {
+    return items.hasOwnProperty(value);
+  };
+  
+  this.add = function(value) {
+  	if (!this.has(value)) {
+      items[value] = value; // added as key/value to help search for value if stored as key too
+      length++;
+      return true;
+  	}
+  	return false;
+  };
+  
+  this.remove = function(value) {
+  	if (this.has(value)) {
+  	  delete items[value];
+  	  length--;
+  	  return true;
+  	}
+  	return false;
+  };
+  
+  this.clear = function() {
+    items = {};	
+    length = 0;
+  };
+  
+  this.size = function() {
+  	return length;
+  	// or return Object.keys(items).length;
+  }
+  
+  this.values = function() {
+    return Object.keys(items);	
+  }
+  
+  this.union = function(otherSet) {
+    var newSet = new Set();
+    
+    var values = this.values();
+    values.forEach(function(val) {
+      newSet.add(val);	
+    });
+    
+    values = otherSet.values();
+    values.forEach(function(val) {
+      newSet.add(val);	
+    }); 
+    
+    return newSet;
+  };
+  
+  this.intersection = function(otherSet) {
+    var intersectionSet = new Set();
+    
+    var values = this.values();
+    values.forEach(function(val) {
+      if (otherSet.has(val)) {
+        intersectionSet.add(val);	
+      }	
+    });
+    
+    return intersectionSet;
+  }
+  
+  this.difference = function(otherSet) {
+    var differenceSet = new Set();
+    
+    var values = this.values();
+    values.forEach(function(val) {
+      if (!otherSet.has(val)) {
+        differenceSet.add(val);	
+      }	
+    });
+    
+    return differenceSet;
+  }, 
+  
+  // checks if this(set) is a subset of otherSet
+  this.subset = function(otherSet) {
+  	
+    if (this.size() > otherSet.size()) {
+      return false;	
+    } else {
+      var values = this.values();
+      values.forEach(function(val) {
+        if (!this.has(val)) {
+          return false;	
+        }  	
+      });
+    return true;
+    }
+  };
+};
+
+var names = new Set();
+names.add('Harry');
+names.add('Ron');
+names.add('Hermoine');
+names.values(); 
+// => [ 'Harry', 'Ron', 'Hermoine' ]
+names.size(); 
+// => 3
+names.remove('Ron'); 
+// => true
+names.values(); 
+// => [ 'Harry', 'Hermoine' ]
+
+var heroes = new Set();
+heroes.add('Superman');
+heroes.add('Batman');
+heroes.add('Captain Planet');
+heroes.add('Harry');
+
+names.intersection(heroes).values();
+// => [ 'Harry']
+
+names.difference(heroes).values();
+// => [ 'Hermoine' ]
+
 // Basic array implementation of Set class
 
 var Set = function() {
