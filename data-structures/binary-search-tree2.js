677716ed09d8be26b2b48d23725c6580a21e1639
Tabitha Blagdon <tjlay23@gmail.com>
2016-05-16 20:13:50 -0700

    Update max, min, traverals, insert methods

---
+++ data-structures/binary-search-tree2.js
@@ -0,0 +1,92 @@
+var BinarySearchTree = function() {
+  
+  var Node = function(key) {
+  	this.key = key;
+  	this.left = null;
+  	this.right = null;
+  }
+  
+  var root = null;
+  
+  var insertNode = function(node, newNode) {
+    if (newNode.key < node.key) {
+      !node.left ? node.left = newNode : insertNode(node.left, newNode);
+    } else {
+      !node.right ? node.right = newNode : insertNode(node.right, newNode);
+    }	
+  };
+  
+  this.insert = function(key) {
+  	var newNode = new Node(key);
+  	
+    if (!root) {
+      root = newNode;
+    } else {
+      insertNode(root, newNode);
+    }
+  };
+  
+  this.search = function(key) {
+  /// in progress	
+  };
+  
+  this.inOrderTraverse = function(callback) {
+  	var inOrderTraverseNode = function(node, callback) {
+  	  if (!node) {
+  	    inOrderTraverseNode(node.left, callback);
+  	    callback(node.key);
+  	    inOrderTraverseNode(node.right, callback);
+  	  }	
+  	};
+    inOrderTraverseNode(root, callback);	
+  };
+  
+  this.preOrderTraverse = function() {
+  	var preOrderTraverseNode = function(node, callback) {
+  	  if (!node) {
+  	  	callback(node.key);
+  	    preOrderTraverseNode(node.left, callback);
+  	    preOrderTraverseNode(node.right, callback);
+  	  }	
+  	};
+    preOrderTraverseNode(root, callback);	
+  };
+  
+  this.postOrderTraverse = function() {
+    var postOrderTraverseNode = function(node, callback) {
+  	  if (!node) {
+  	    postOrderTraverseNode(node.left, callback);
+  	    postOrderTraverseNode(node.right, callback);
+  	  	callback(node.key);
+  	  }	
+  	};
+    postOrderTraverseNode(root, callback);
+  };
+  
+  this.min = function() {
+    if (root) {
+      var current = root;
+      while (current.left) {
+        current = current.left;  	
+      }
+      return current.key;
+    }
+    return null;	
+  };
+  
+  this.max = function() {
+    if (root) {
+      var current = root;
+        while (current.right) {
+          current = current.right;  	
+        }
+        return current.key;
+    }
+    return null;	
+  };
+  
+  this.remove = function(key) {
+  /// in progress	
+  };
+  
+};
