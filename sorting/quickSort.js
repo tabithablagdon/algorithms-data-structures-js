e8658711539ac97de8041445ab3ccd801c11d2a1
Tabitha Blagdon <tjlay23@gmail.com>
2016-05-20 18:31:06 -0700

    Update quickSort.js

--- sorting/quickSort.js
+++ sorting/quickSort.js
@@ -1,6 +1,9 @@
-// quicksort implementation
 
-var qSort = function(arr) {
+////////////////////////////////////////////////////////////////////////////////////////////
+// Basic quicksort implementation with pivot as first value                               //
+////////////////////////////////////////////////////////////////////////////////////////////
+
+var quickSort = function(arr) {
   if (arr.length === 0) {
     return [];	
   }	
@@ -10,8 +13,27 @@ var qSort = function(arr) {
   for (var i = 1, len = arr.length; i < len; i++) {
     arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i]);
   }
-  return qSort(left).concat(pivot, qSort(right));
+  return quickSort(left).concat(pivot, quickSort(right));
 };
 
-qSort([6, 23, 2, 1, 4, -10]);
+quickSort([6, 23, 2, 1, 4, -10]);
 // => [ -10, 1, 2, 4, 6, 23 ]
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// quickSort with pivot as the middle number - better if starting with almost sorted list //
+////////////////////////////////////////////////////////////////////////////////////////////
+
+// var quickSort = function(arr) {
+//   if (arr.length === 0) {
+//     return [];	
+//   }
+//   var pivot = arr.splice(Math.floor(arr.length/2), 1), 
+//       left = [], 
+//       right = [];
+ 
+//   for (var i = 0; i < arr.length; i++) {
+//     arr[i] < pivot ? left.push(arr[i]) : right.push(arr[i]);
+//   }
+  
+//   return quickSort(left).concat(pivot, quickSort(right));
+// };
