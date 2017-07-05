eaa5336fd9bd5ccfa97074325302a00e20d58f22
Tabitha Blagdon <tjlay23@gmail.com>
2017-07-05 09:01:03 -0700

    Create mergeSortedArrays.js

---
+++ recursion/mergeSortedArrays.js
@@ -0,0 +1,42 @@
+// Question (from InterviewCake): We have our lists of orders sorted numerically already, in arrays. Write a function to merge our arrays of orders into one sorted array.
+
+var myArray     = [3, 4, 6, 10, 11, 15];
+var alicesArray = [1, 5, 8, 12, 14, 19];
+
+// Recursive solution
+
+function mergeArrays(arrayA, arrayB, sortedArray) {
+  sortedArray = sortedArray || [];
+  const lenA = arrayA.length;
+  const lenB = arrayB.length;
+  
+  // Base case
+  if (lenA === 0 && lenB === 0) {
+    return sortedArray;
+  }
+  
+  // Recursive case
+  if (lenA > 0 && lenB > 0) {
+      const nextA = arrayA[0];
+      const nextB = arrayB[0];
+      
+      if (nextA === nextB) {
+        sortedArray.push(nextA);
+        sortedArray.push(nextB);
+        
+        return mergeArrays(arrayA.slice(1), arrayB.slice(1), sortedArray);
+      } else if (nextA > nextB) {
+        sortedArray.push(nextB);
+        
+        return mergeArrays(arrayA, arrayB.slice(1), sortedArray);
+      } else {
+        sortedArray.push(nextA);
+        
+        return mergeArrays(arrayA.slice(1), arrayB, sortedArray);
+      }
+  } else {
+    return lenA > 0 ? sortedArray.concat(arrayA) : sortedArray.concat(arrayB);
+  }
+}
+
+mergeArrays(myArray, alicesArray)
