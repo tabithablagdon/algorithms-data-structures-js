ee4cf8d062ddc52a66f4a2881a26600173550aa7
Tabitha Blagdon <tjlay23@gmail.com>
2018-01-06 16:53:19 -0800

    Update multiply.js

--- recursion/multiply.js
+++ recursion/multiply.js
@@ -1,7 +1,30 @@
 // Write an algorithm multiply that multiples two positive integers and doesn't use the * operator. All other operators can be used.
 
-// Iterative solution
+// Recursive solution
+var multiply = function(a, b) {
+  // return 0 if one of the numbers is 0
+  if (!a || !b) return 0;
+  // return the other number is one of the numbers is 1
+  if (a === 1 || b === 1) return a === 1 ? b : a;
+  
+  let smallerNum = a < b ? a : b;
+  let largerNum = b < a ? a : b;
+  
+  return multiplyHelper(largerNum, smallerNum);
+};
 
+function multiplyHelper(largerNum, smallerNum) {
+  if (smallerNum === 0) return 0;
+  if (smallerNum === 1) return largerNum;
+  
+  let halfProduct = multiplyHelper(largerNum, smallerNum >> 1);
+  
+  return halfProduct += (smallerNum % 2 === 0 ? halfProduct : halfProduct + largerNum);
+};
+
+multiply(20, 3); // 60
+
+// Iterative solution
 var multiply = function(a, b) {
   // return 0 if one of the numbers is 0
   if (!a || !b) return 0;
